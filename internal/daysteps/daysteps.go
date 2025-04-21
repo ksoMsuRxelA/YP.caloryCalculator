a7a7ea85e108fe1a84c2781b1f97421f12e99dab
Ruslan Moskvitin <ra.moskvitin@yandex.ru>
2025-04-21 16:48:03 +0300

    feat: implement DayActionInfo() function

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,16 +1,19 @@
 package daysteps
 
 import (
-	"time"
-	"strings"
-	"strconv"
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 var (
 	ErrInvalidInput = errors.New("invalid format of input was provided")
-	ErrConvToInt = errors.New("couldn't convert to an integer")
-	ErrZeroValue = errors.New("can't be zero")
+	ErrConvToInt    = errors.New("couldn't convert to an integer")
+	ErrSubZeroValue = errors.New("can't be less or equal to zero")
 )
 
 const (
@@ -23,25 +26,48 @@ const (
 func parsePackage(data string) (int, time.Duration, error) {
 	parts := strings.Split(data, ",")
 	if len(parts) != 2 {
-		return 0, 0, fmt.Errorf("there was a problem when calling \"parsePackage()\" with argument \"%s\": %w\n", data, ErrInvalidInput)
+		err := fmt.Errorf("there was a problem when calling \"parsePackage()\" with argument \"%s\": %w\n", data, ErrInvalidInput)
+		return 0, 0, err
 	}
 
 	steps, err := strconv.Atoi(parts[0])
 	if err != nil {
-		return 0, 0, fmt.Errorf("there was a problem when calling \"parsePackage()\" with this argument's part \"%s\": %w\n", parts[0], ErrConvToInt)
+		err = fmt.Errorf("there was a problem when calling \"parsePackage()\" with this argument's part \"%s\": %w\n", parts[0], ErrConvToInt)
+		return 0, 0, err
 	}
 	if steps <= 0 {
-		return 0, 0, fmt.Errorf("there was a problem when calling \"parsePackage()\" with this argument's part \"%s\": %w\n", parts[0], ErrZeroValue)
+		err = fmt.Errorf("there was a problem when calling \"parsePackage()\" with this argument's part \"%s\": %w\n", parts[0], ErrSubZeroValue)
+		return 0, 0, err
 	}
-	
+
 	duration, err := time.ParseDuration(parts[1])
 	if err != nil {
-		return 0, 0, fmt.Errorf("there was a problem when calling \"parsePackage()\" with this argument's part \"%s\": %w\n", parts[1], ErrInvalidInput)
+		err = fmt.Errorf("there was a problem when calling \"parsePackage()\" with this argument's part \"%s\": %w\n", parts[1], ErrInvalidInput)
+		return 0, 0, err
 	}
 
 	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if steps <= 0 {
+		return ""
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	distInM := stepLength * float64(steps)
+	distInKm := distInM / mInKm
+
+	burnedCals, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distInKm, burnedCals)
 }
